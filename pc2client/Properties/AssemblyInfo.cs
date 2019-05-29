2c1de30538635e1f48e37a464f946a7a942a09f7
DarkMorford <DarkMorford@users.noreply.github.com>
2019-05-29 10:19:09 -0700

    Update application metadata

--- pc2client/Properties/AssemblyInfo.cs
+++ pc2client/Properties/AssemblyInfo.cs
@@ -16,12 +16,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("pc2client")]
+[assembly: AssemblyTitle("Project CARS 2 Telemetry Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("pc2client")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("LoadingReadyRun Community")]
+[assembly: AssemblyProduct("24 Hours of LRRmans")]
+[assembly: AssemblyCopyright("Copyright © LoadingReadyRun Community 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -58,5 +58,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
